b0f05111f2aec3be003083091a12b48844ec5f22
Charlie Oh <mun5424@gmail.com>
2018-03-20 14:55:06 -0700

    fully functional

--- data/friends.js
+++ data/friends.js
@@ -1,19 +1,25 @@
-var friendArray = [ 
-{
-    name:"Ahmed",
-    photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    scores:[
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
-      ]
-  }
+var friendArray = [
+    {
+        name: "Charlie",
+        photo: "https://imgur.com/r/cats/YoWVOVN",
+        scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+    },    
+    {
+        name: "Taylor",
+        photo: "https://i.imgur.com/rsD0RUq.jpg",
+        scores: [1, 3, 3, 5, 2, 3, 5, 1, 5, 4]
+    },
+    {
+        name: "Sean",
+        photo: "https://i.imgur.com/Cxagv.jpg",
+        scores: [3, 1, 2, 4, 1, 4, 5, 1, 5, 1]
+    },
+    {
+        name: "Daniel",
+        photo: "https://i.imgur.com/Tgqh5Hn.jpg?1",
+        scores: [1, 3, 5, 3, 1, 2, 2, 2, 1, 1,]
+    },
 ]
 module.exports = friendArray;
+
+
--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -7,6 +7,7 @@ module.exports = function(app) {
   });
 
   app.post("/api/friends", function(req, res) {
+      res.send(friendsData); 
       friendsData.push(req.body); 
   });
 
