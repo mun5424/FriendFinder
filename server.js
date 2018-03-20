a7bdbb7ea9cc3a6c997ae8c02237348fbd338b6d
Charles Oh <mun5424@gmail.com>
2018-03-20 00:20:12 -0700

    added all logic. still not complete

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
-var questions = [
-    "i am a blunt person. ",
-    "i take criticism too seriously.",
-    "I am a perfectionist. ",
-    "It is okay to blame someone on a mistake if they do not find out. ",
-    "I prefer dating at daytime as opposed to nighttime. ",
-    "I try my best to adjust myself to my surrroundings. ",
-    "I daydream a lot of times.",
-    "I do not get upset easily.",
-    "I like to go out on the weekends. ",
-    "I prefer being truthful than to lie to keep a relationship."
-]
+var express = require("express");
+var bodyParser = require("body-parser");
 
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+require("./routing/apiRoutes")(app);
+require("./routing/htmlRoutes")(app);
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
